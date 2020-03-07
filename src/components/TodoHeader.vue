52e0d51c12a30c91b53ddf15887fd36d2ce72177
yellowduckgh <honest95@gmail.com>
2020-03-07 15:07:08 +0900

    todoheader 수정

--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -11,15 +11,15 @@ h1 {
 <script>
 export default {
 	created() {
-		var h1 = document.querySelector('h1');
+		//var h1 = document.querySelector('h1');
 		//console.log('created', h1);
 	},
 	beforeMount() {
-		var h1 = document.querySelector('h1');
+		//var h1 = document.querySelector('h1');
 		//console.log('beforeMount', h1);
 	},
 	mounted() {
-		var h1 = document.querySelector('h1');
+		//var h1 = document.querySelector('h1');
 		//console.log('mounted', h1);
 	},
 };
